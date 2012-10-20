67ea36f461881d1610d3bda784f3481ae7b380f4
Nicholas Cox <iamnicholascox@gmail.com>
2012-10-20 08:26:03 -0700

    Initial commit with gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codealong::Application.config.secret_token = 'da77aa4b2a9f32fb110c2be9c943f46e6d8e2255217e4197dfcadcdc35f61b2ec7498a4be8076dd8ffe0bcc7287ac0d1852a75ba22450ff38f708697f1444f5d'
