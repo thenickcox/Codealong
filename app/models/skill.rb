9d5bb4f36e273fe2b0fbcd259db7695553626ea9
David Pierce <david.dean.pierce@gmail.com>
2012-10-20 22:25:52 -0700

    Add pre-save callback to normalize search field

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,5 +3,13 @@ class Skill < ActiveRecord::Base
   has_many :user_skills
   has_many :skills, through: :user_skills
 
-  attr_accessible :name
+  # Callbacks
+  before_save :normalize_skill_name
+
+  attr_accessible :name, :normalized_name
+
+  private
+    def normalize_skill_name(skill)
+      skill.normalize_skill_name = skill.name.strip.downcase()
+    end
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe Skill do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "callbacks" do
+    describe "before_save" do
+      it "should normalize the skill name" do
+        skill = Skill.new
+        skill.name = "  A Skill "
+        skill.save
+
+        skill.normalized_name.should equal "a skill"
+      end
+    end
+  end
 end
