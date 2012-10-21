8f6cf958605e0bdf7253675307cf36553d86eca9
David Pierce <david.dean.pierce@gmail.com>
2012-10-21 00:43:38 -0700

    Begin with initial migration for new fields

---
+++ db/migrate/20121021073129_add_new_fields_to_user.rb
@@ -0,0 +1,17 @@
+class AddNewFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :company, :string
+    add_column :users, :industry, :string
+    add_column :users, :years_of_experience, :integer
+    add_column :users, :phone, :string
+    add_column :users, :skype, :string
+    add_column :users, :facebook, :string
+    add_column :users, :twitter, :string
+    add_column :users, :address1, :string
+    add_column :users, :address2, :string
+    add_column :users, :city, :string
+    add_column :users, :state_province, :string
+    add_column :users, :zip, :string
+    add_column :users, :country, :string
+  end
+end
